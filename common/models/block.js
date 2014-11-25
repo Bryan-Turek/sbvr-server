fdff2e2c871ce76bc8617b3a9470aeb4959837ba
Bryan Turek <bryan@Bryans-MacBook-Pro.local>
2014-11-24 20:28:51 -0500

    fix

--- common/models/block.js
+++ common/models/block.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto');
 
 module.exports = function(Block) {
-    Block.definition.properties.revision.default = function() {
-      return crypto.createHash('md5').update((new Date()).toString()).digest('hex');
-    }
+  Block.definition.properties.revision.default = function() {
+    return crypto.createHash('md5').update((new Date()).toString()).digest('hex');
+  }
 }
