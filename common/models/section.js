19a5bb2977dfdabd58412a15bd275fcd8c041485
Bryan Turek <bryan@Bryans-MacBook-Pro.local>
2014-11-24 20:08:13 -0500

    added in section for block

--- common/models/section.js
+++ common/models/section.js
@@ -1,9 +1,9 @@
 var crypto = require('crypto');
 
-module.exports = function(Project) {
+module.exports = function(Section) {
 
   //Create default revision
-  Project.definition.properties.revision.default = function() {
+  Section.definition.properties.revision.default = function() {
     return crypto.createHash('md5').update((new Date()).toString()).digest('hex');
   }
 }
