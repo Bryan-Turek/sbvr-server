0cda9bf070d977854048a09ccc675135dc24450d
Bryan Turek <bryan@Bryans-MacBook-Pro.local>
2014-11-24 20:19:54 -0500

    Added default revision to section.js

--- common/models/section.js
+++ common/models/section.js
@@ -1,8 +1,6 @@
 var crypto = require('crypto');
 
 module.exports = function(Section) {
-
-  //Create default revision
   Section.definition.properties.revision.default = function() {
     return crypto.createHash('md5').update((new Date()).toString()).digest('hex');
   }
